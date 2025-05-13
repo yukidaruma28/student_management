ac40a3ad28156d3aa0087539f554a746a43d1424
yukidaruma28 <nakata@snowmans1630.com>
2025-05-13 19:57:27 +0900

    ensure line で末尾に改行を入れる設定をしました

---
+++ src/test/java/studentManagementSystem/testDemo/Controller/StudentControllerTest.java
@@ -0,0 +1,61 @@
+package studentManagementSystem.testDemo.Controller;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import java.util.List;
+
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import studentManagementSystem.testDemo.data.Student;
+import studentManagementSystem.testDemo.domain.StudentDetail;
+import studentManagementSystem.testDemo.service.StudentService;
+
+
+@WebMvcTest(StudentController.class)
+class StudentControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+//  @MockBean
+  private StudentService service;
+
+  private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+  @Test
+  void 受講生詳細の一覧検索が実行できて空のリストが返ってくること() throws Exception {
+    when(service.searchStudentList()).thenReturn(List.of(new StudentDetail()));
+
+    mockMvc.perform(get("/studentList"))
+        .andExpect(status().isOk())
+        .andExpect(content().json("[]"));
+
+    verify(service, times(1)).searchStudentList();
+  }
+
+  @Test
+  void 受講生詳細の受験生でID以外に数字を用いた時に入力チェックにかかること() {
+    Student student = new Student();
+
+    student.setStudentId("テスト");
+
+    Set<ConstraintViolation<Student>> violations = validator.validate(student);
+    Assertions.assertEquals("1" ,violations.size());
+
+  }
+}
